3fb02ed5a42e86fba2dc41054952cfeefc2faa68
Mateo Torres <torresmateo@gmail.com>
2019-02-27 17:15:28 +0000

    added support for Initial Concentration

--- create_projects.py
+++ create_projects.py
@@ -4,12 +4,9 @@ import numpy as np
 import itertools
 import argparse
 import os
-import shutil
 
 aparser = argparse.ArgumentParser()
 required_arguments = aparser.add_argument_group('required arguments')
-required_arguments.add_argument('--sample-projects', '--sp',
-                                help='a path to the folder in which the CROMO project is stored', required=True)
 required_arguments.add_argument('--output-dir', '--od',
                                 help='a path to a suitable folder (ideally empty) where the new configurations will '
                                      'be stored', required=True)
@@ -18,26 +15,30 @@ required_arguments.add_argument('--ranges', '--r',
 args = aparser.parse_args()
 
 out_dir = os.path.realpath(os.path.expandvars(os.path.expanduser(args.output_dir)))
-proj_dir = os.path.realpath(os.path.expandvars(os.path.expanduser(args.sample_projects)))
+proj_dir = os.path.dirname(os.path.realpath(os.path.expandvars(os.path.expanduser(__file__))))
 print(proj_dir)
 
 if not os.path.exists(out_dir):
     print('Output directory does not exist, attempting to create...')
     os.mkdir(out_dir)
-os.system(f'copy run_hydrus.bat {out_dir}')
-os.system(f'copy return.txt {out_dir}')
+if os.name == 'nt':
+    os.system(f'copy run_hydrus.bat {out_dir}')
+    os.system(f'copy return.txt {out_dir}')
+else:
+    os.system(f'cp run_hydrus.bat {out_dir}')
+    os.system(f'cp return.txt {out_dir}')
 
 print('reading ranges...')
 ranges = pd.read_csv(args.ranges)
 
-supported_variables = ['Bulk_d', 'DisperL', 'DisperT', 'DifW', 'SnkL1']
+supported_variables = ['Bulk_d', 'DisperL', 'DisperT', 'DifW', 'SnkL1', 'Conc']
 
 data = {}
 
 for _, r in ranges.iterrows():
     data[r.variable] = list(np.arange(r.start, r.stop, r.step))
 
-template = {'CROMO': '''Pcp_File_Version=4
+template = {'CROMO': {'selector':'''Pcp_File_Version=4
 *** BLOCK A: BASIC INFORMATION *****************************************
 Heading
 Welcome to HYDRUS
@@ -86,7 +87,7 @@ TPrint(1),TPrint(2),...,TPrint(MPL)
       tPulse
          24
 *** END OF INPUT FILE 'SELECTOR.IN' ************************************
-'''}
+''', 'domain': open(os.path.join(proj_dir, 'CROMO', 'PRE', 'DOMAIN.DAT')).read()} }
 
 
 configuration_details = ""
@@ -94,12 +95,22 @@ configuration_details = ""
 for e, configuration in enumerate(itertools.product(*[data[k] for k in supported_variables])):
     print(f'Writing configuration {e}')
     config_dir = os.path.join(out_dir, f'configuration_{e}\\')
-    print(f'copy /E {os.path.join(proj_dir,"CROMO","PRE")} {config_dir}')
-    os.system(f'xcopy /E {os.path.join(proj_dir,"CROMO","PRE")} {config_dir}')
+    if os.name == 'nt':
+        os.system(f'xcopy /E {os.path.join(proj_dir,"CROMO","PRE")} {config_dir}')
+    else:
+        os.system(f'cp -rf {os.path.join(proj_dir, "CROMO", "PRE")} {config_dir}')
+    
     selector = open(os.path.join(config_dir, 'SELECTOR.IN'), 'w')
-    selector.write(template['CROMO'].format(**{x: y for x, y in zip(supported_variables, configuration)}))
+    selector.write(template['CROMO']['selector'].format(**{x: y for x, y in zip(supported_variables, configuration)}))
     selector.close()
-    configuration_details += str({x: y for x, y in zip(supported_variables, configuration)}) + '\n'
+    
+    domain = open(os.path.join(config_dir, 'DOMAIN.DAT'), 'w')
+    domain.write(template['CROMO']['domain'].format(**{x: y for x, y in zip(supported_variables, configuration)}))
+    domain.close()
+    
+
+    configuration_details += f'configuration {e} : ' + \
+                             str({x: y for x, y in zip(supported_variables, configuration)}) + '\n'
 
 configurations = open(os.path.join(out_dir, 'configurations.txt'), 'w')
 configurations.write(configuration_details)
